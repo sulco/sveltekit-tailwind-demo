2d48eddc21e4a68c4641f85746ffab595ffdb950
 Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-02-19 21:05:18 +0100

    some lorem ipsum

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<main action="" class="mt-4 m-auto shadow-md p-4 rounded max-w-lg text-neutral-700 bg-gray-100">
+  <h1 class="text-2xl font-light">Hello world</h1>
+  <p class="my-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia eius consectetur adipisci autem. Quia magni, earum obcaecati vero pariatur quisquam enim itaque accusamus neque, ipsum autem minus libero consectetur deleniti!</p>
+  <p class="my-3">Animi, quasi eum! Veritatis repellat deserunt fugit sapiente minus iusto voluptates id aliquam, beatae, ducimus facere odio natus obcaecati molestias odit quae velit eveniet cumque praesentium dolores inventore dolorem animi.</p>
+  <p class="my-3">Possimus iste nulla eius nesciunt at, molestias dolorem officia ea repudiandae fugit! Voluptatum fugit odio eos beatae sequi unde qui dolorem sunt atque earum odit hic nobis, neque voluptatem rem!</p>
+</main>
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.gray.100);
+    background-color: theme(colors.gray.200);
   }
 </style>
